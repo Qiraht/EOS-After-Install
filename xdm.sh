912f7d8a10de1fe521c0e21920419e49150dd0f0
Thariq Aulia Akbar <thariq2712@gmail.com>
2020-08-17 18:23:44 +0700

    Add Xdm Script

---
+++ xdm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Author : Thariq Aulia Akbar
+# Github : github.com/Qiraht
+#
+# Script to install XDM
+# On any distro
+
+Directory="$HOME/Downloads/Programs/"
+
+if [ -d "$Directory" ] 
+then
+    echo "Directory exists." 
+else
+    echo -e "Directory does not exists. \nCreating a new one"
+    mkdir "$Directory"
+fi
+
+wget -c "https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz" -P "$Directory"
+
+Path="$Directory/xdm-setup-7.2.11"
+
+mkdir $Path
+
+tar -C $Path -xf $Directory/xdm-setup-7.2.11.tar.xz
+
+sudo bash $Path/install.sh
